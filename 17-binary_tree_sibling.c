525ec363d9fe4a9c67b5093d4aedc04781b73c9a
charlemagne <agbolossouc@gmail.com>
2022-09-23 10:53:56 -0700

    search sibling

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * *binary_tree_sibling - find sibling
+ *
+ * @node: sibling node
+ * Return: pointer
+ */
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+
+	if (node == node->parent->left)
+		return (node->parent->right);
+
+	return (node->parent->left);
+}
